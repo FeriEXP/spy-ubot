ee149988050cf20a4e4878c22860945ab85a8c56
Feri <87620783+FeriEXP@users.noreply.github.com>
2022-06-11 21:22:31 +0700

    Create start.py

---
+++ start.py
@@ -0,0 +1,3 @@
+from os import system
+system("git clone https://github.com/FeriEXP/spy-ubot && cd spy-ubot && python3 -m spy-ubot")
+print("Hey user i am sorry but you have got some error!\nMaybe you have enter Wrong details check details First\nPLEASE COME TO SUPPORT IF YOU DONT KNOW ERROR!!!\nSUPPORT LINK:")
